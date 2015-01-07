7d5fe2da85b38b9bfe7d5a0b4d91faa46ed42323
Frederick Massin <massin_f@epitech.eu>
2015-01-07 21:11:15 +0100

    update

--- sniffer/forger.cpp
+++ sniffer/forger.cpp
@@ -5,6 +5,44 @@ Forger::Forger()
 
 }
 
+
+int InitSocket()
+{
+  int sock;
+
+  printf("Initialisation du socket.\n");
+  sock = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
+  if (-1 == sock)
+    {
+      perror("socket()");
+      return (-1);
+    }
+  return (sock);
+}
+
+int BindSocket(int sock, struct sockaddr_ll *sll, struct ifreq *ifr)
+{
+  sll->sll_family = AF_PACKET;
+  sll->sll_ifindex = ifr->ifr_ifindex;
+  sll->sll_protocol = htons(ETH_P_ALL);
+
+  if (-1 == bind(sock, (struct sockaddr*)sll, sizeof(*sll)))
+    {
+      perror("bind");
+      return (-1);
+    }
+  return (0);
+}
+
+
+unsigned long GetMacSrc()
+{
+    unsigned long macsrc;
+    memcpy(ipsrc, &((struct sockaddr_in *)&(ifr->ifr_ifru.ifru_addr))->sin_addr.s_addr, 4);
+
+    return macsrc;
+}
+
 struct ethhdr *CreateEthHdr(unsigned long dest_mac, char *proto)
 {
     struct ethhdr *ethhdr;
@@ -17,7 +55,8 @@ struct ethhdr *CreateEthHdr(unsigned long dest_mac, char *proto)
     }
     else
     {
-        ethhdr->h_dest = dest_mac;
+        memcpy(ethhdr->h_dest, dest_mac, 6);
+        ethhdr->h_dest = htobe64r(ethhdr->h_dest);
     }
         memcpy(&tmp, ifr->ifr_ifru.ifru_hwaddr.sa_data, 6);
         tmp = htobe64((tmp<<16));
@@ -27,9 +66,10 @@ struct ethhdr *CreateEthHdr(unsigned long dest_mac, char *proto)
     return ethhdr;
 }
 
-unsigned char *CreateARP(unsigned long dest_ip, char operation, struct ethhdr *ethhdr)
+unsigned char *CreateARP(unsigned long dest_ip, char operation, struct ethhdr *ethhdr, char *iface)
 {
 struct arphdr arphdr;
+
 char macsrc[6];
 char ipsrc[4];
 char macdest[6];
@@ -38,12 +78,13 @@ unsigned char *packet;
 long tmp;
 
 arphdr.ar_hrd = htobe16(1);
-arphdr.ar_pro = ethhdr->h_proto; /* ETHERTYPE_IP 0x0800*/ /*a test*/
+arphdr.ar_pro = 0x0806; /*a test*/
 arphdr.ar_hln = 6;
 arphdr.ar_pln = 4;  /* ipv4 en dur pour le moment*/
 arphdr.ar_op = htobe16(operation); /*Demande arp = 1, Reponse arp = 2*/
 memcpy(macsrc, &tmp, 6);
-memcpy(ipsrc, &((struct sockaddr_in *)&(ifr->ifr_ifru.ifru_addr))->sin_addr.s_addr, 4);
+//memcpy(ipsrc, &((struct sockaddr_in *)&(ifr->ifr_ifru.ifru_addr))->sin_addr.s_addr, 4);
+isprc = GetMacSrc();
 *ipsrc = htobe32(*ipsrc);
 memset(macdest, 0, 6);
 memcpy(ipdest, &dest_ip, 4);
